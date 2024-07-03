74e8846777ca82131a18a1b5870c0da20992a6c3
Obada-Badee <obadabadee3@hotmail.com>
2024-07-03 23:03:06 +0300

    Fix lint

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,14 @@
 export default function updateStudentGradeByCity(list, city, grades) {
-    if (!Array.isArray(list) || !Array.isArray(grades)) {
-      return [];
-    }
-  
-    const newlist = list.filter((student) => student.location === city)
-      .map((student) => {
-        const item2 = grades.find((student2) => student.id === student2.studentId);
-  
-        return { ...student, grade: item2 ? item2.grade : 'N/A' };
-      });
-  
-    return newlist;
+  if (!Array.isArray(list) || !Array.isArray(grades)) {
+    return [];
   }
+
+  const newlist = list.filter((student) => student.location === city)
+    .map((student) => {
+      const item2 = grades.find((student2) => student.id === student2.studentId);
+
+      return { ...student, grade: item2 ? item2.grade : 'N/A' };
+    });
+
+  return newlist;
+}
